ce3fcd8a5926fe4ea5ec3a145e1bc0ccb9ad6f6a
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-15 14:58:21 +0200

    feat(admin): added logout button and used material ui

--- libs/core/web/src/lib/pages/admin/Admin.tsx
+++ libs/core/web/src/lib/pages/admin/Admin.tsx
@@ -1,11 +1,50 @@
+import { Box } from '@mui/material';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 /* eslint-disable-next-line */
 export interface AdminProps {}
 
 export function Admin(props: AdminProps) {
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+  const logout = (): void => {
+    axios
+      .post('/api/logout')
+      .then((response) => {
+        if (response.status === 200) {
+          navigate('/');
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
   return (
-    <div>
-      <h1>Welcome to Admin!</h1>
-    </div>
+    <Box component="div">
+      <Typography variant="h2" component="div">
+        Administration
+      </Typography>
+
+      <Button variant="outlined" onClick={logout}>
+        {'Déconnexion'}
+      </Button>
+      <Typography
+        gutterBottom
+        variant="body1"
+        sx={{
+          color: '#d32f2f',
+          height: '1rem',
+        }}
+      >
+        {error}
+      </Typography>
+    </Box>
   );
 }
 
