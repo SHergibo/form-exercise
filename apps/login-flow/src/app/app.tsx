b29de4bc6e239b8eb95917a46e98e16c306bf471
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-15 17:01:23 +0200

    feat(app): added I18nextProvider

--- apps/login-flow/src/app/app.tsx
+++ apps/login-flow/src/app/app.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Admin, Login } from '@form-exercise/core/web';
+import i18n from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import { initI18Next } from './i18next/i18next.config';
+
+initI18Next();
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </I18nextProvider>
   );
 }
 
