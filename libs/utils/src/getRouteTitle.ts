a4a836ec0daa95884fe02569915e636ed2eb9e7e
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-19 14:10:37 +0200

    refactor(getroutetitle): used routesArray constant from lib utils constants

--- libs/utils/src/getRouteTitle.ts
+++ libs/utils/src/getRouteTitle.ts
@@ -1,14 +1,12 @@
 import { i18nKeys } from '@form-exercise/i18n';
+import { routesArray, IRoutesArray } from '@form-exercise/utils';
 
 interface IGetRouteTitle {
   pathname: string;
 }
 
 export const getRouteTitle = ({ pathname }: IGetRouteTitle): string => {
-  const routes = [
-    { path: '/', routeName: i18nKeys.menu.title.login },
-    { path: '/admin', routeName: i18nKeys.menu.title.admin },
-  ];
+  const routes: IRoutesArray[] = routesArray;
   const routeIndex = routes.findIndex((route) => route.path === pathname);
 
   if (routeIndex !== -1) {
