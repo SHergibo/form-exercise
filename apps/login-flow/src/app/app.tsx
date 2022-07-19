b51c53ec18f0ab618b2ddb376216b36e0c9ff4a8
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-19 10:18:00 +0200

    feat(app.tsx): added AuthContext

--- apps/login-flow/src/app/app.tsx
+++ apps/login-flow/src/app/app.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Admin, Login, NotFound } from '@form-exercise/core/web';
+import { Admin, AuthContext, Login, NotFound } from '@form-exercise/core/web';
 import i18n from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import { initI18Next } from './i18next/i18next.config';
@@ -12,20 +12,22 @@ export function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter>
-        <Routes>
-          <Route element={<MenuApp />}>
-            <Route path="/" element={<Login />} />
-            <Route
-              path="/admin"
-              element={
-                <ErrorBoundary>
-                  <Admin />
-                </ErrorBoundary>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <AuthContext>
+          <Routes>
+            <Route element={<MenuApp />}>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/admin"
+                element={
+                  <ErrorBoundary>
+                    <Admin />
+                  </ErrorBoundary>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </AuthContext>
       </BrowserRouter>
     </I18nextProvider>
   );
