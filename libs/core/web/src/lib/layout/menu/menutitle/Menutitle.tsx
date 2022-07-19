30988fe49e64af90e3c676044ec0e53daaa8f080
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-19 13:30:30 +0200

    refactor(menutitle): used getRouteTitle to show menu title instead of if statement

--- libs/core/web/src/lib/layout/menu/menutitle/Menutitle.tsx
+++ libs/core/web/src/lib/layout/menu/menutitle/Menutitle.tsx
@@ -1,17 +1,15 @@
 import Typography from '@mui/material/Typography';
 import { useTranslation } from 'react-i18next';
-import { i18nKeys } from '@form-exercise/i18n';
+import { useLocation } from 'react-router-dom';
+import { getRouteTitle } from '@form-exercise/utils';
 
 export function Menutitle() {
   const { t } = useTranslation();
+  const location = useLocation();
 
   return (
     <Typography variant="h6" sx={{ flexGrow: 1 }}>
-      {location.pathname === '/' && t(i18nKeys.menu.title.login)}
-      {location.pathname === '/admin' && t(i18nKeys.menu.title.admin)}
-      {location.pathname !== '/admin' &&
-        location.pathname !== '/' &&
-        t(i18nKeys.menu.title.notFound)}
+      {t(getRouteTitle(location))}
     </Typography>
   );
 }
