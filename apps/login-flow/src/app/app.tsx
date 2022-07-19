30315c0ce4df29b9efdb51cee4b86a6be7ccde38
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-19 17:02:40 +0200

    feat(app.tsx): added ThemeContext

--- apps/login-flow/src/app/app.tsx
+++ apps/login-flow/src/app/app.tsx
@@ -5,6 +5,7 @@ import { I18nextProvider } from 'react-i18next';
 import { MenuApp } from '@form-exercise/core/web';
 import ErrorBoundary from 'libs/core/web/src/lib/error-handler/error-boundary/ErrorBoundary';
 import { initI18Next } from '@form-exercise/i18n';
+import { ThemeContext } from '@form-exercise/ui';
 
 initI18Next();
 
@@ -12,20 +13,22 @@ export function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter>
-        <Routes>
-          <Route element={<MenuApp />}>
-            <Route path="/" element={<Login />} />
-            <Route
-              path="/admin"
-              element={
-                <ErrorBoundary>
-                  <Admin />
-                </ErrorBoundary>
-              }
-            />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <ThemeContext>
+          <Routes>
+            <Route element={<MenuApp />}>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/admin"
+                element={
+                  <ErrorBoundary>
+                    <Admin />
+                  </ErrorBoundary>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </ThemeContext>
       </BrowserRouter>
     </I18nextProvider>
   );
