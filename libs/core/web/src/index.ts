0f561b126fb3b26a7fe9f3ac9649b1afa9afc72d
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-20 13:18:20 +0200

    feat(lib core/web): created routes lib

--- libs/core/web/src/index.ts
+++ libs/core/web/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/error-handler/error-boundary/ErrorBoundary';
 export * from './lib/layout/menu/MenuApp';
 export * from './lib/pages/admin/Admin';
 export * from './lib/pages/login/Login';
+export * from './lib/routes';
