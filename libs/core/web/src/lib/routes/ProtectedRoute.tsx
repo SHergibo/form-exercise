17da77ab7b9c4056bee7be26511e0ed4d0d29962
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-20 17:09:08 +0200

    feat(protectedroute): created ProtectedRoute component

---
+++ libs/core/web/src/lib/routes/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import useAuthContext from 'libs/utils/src/hooks/useAuthContext';
+import { Navigate, Outlet } from 'react-router-dom';
+import { AppRoute, getRoutePath } from './routes';
+
+export const ProtectedRoute = () => {
+  const { isLogged } = useAuthContext();
+
+  return isLogged ? (
+    <Outlet />
+  ) : (
+    <Navigate replace to={getRoutePath(AppRoute.ADMIN)} />
+  );
+};
