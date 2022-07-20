89bb9d20853e6434638ac1defeca8db47c8c5bb7
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-20 10:25:18 +0200

    refactor(getroutetitle): change IRouteArray interface to RouteArray

--- libs/utils/src/getRouteTitle.ts
+++ libs/utils/src/getRouteTitle.ts
@@ -1,12 +1,12 @@
 import { i18nKeys } from '@form-exercise/i18n';
-import { routesArray, IRoutesArray } from '@form-exercise/utils';
+import { routesArray, RoutesArray } from '@form-exercise/utils';
 
 interface IGetRouteTitle {
   pathname: string;
 }
 
 export const getRouteTitle = ({ pathname }: IGetRouteTitle): string => {
-  const routes: IRoutesArray[] = routesArray;
+  const routes: RoutesArray[] = routesArray;
   const routeIndex = routes.findIndex((route) => route.path === pathname);
 
   if (routeIndex !== -1) {
