a1673bbcfbaecd12e0399dd4b5e9254bf70ae9c7
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-20 15:45:57 +0200

    feat(useauthcontext): added useAuhtContext hook logic

--- libs/utils/src/hooks/useAuthContext.tsx
+++ libs/utils/src/hooks/useAuthContext.tsx
@@ -1,1 +1,6 @@
-export const temporary = 'temp';
+import { IsLoggedContext } from '@form-exercise/core/web';
+import { useContext } from 'react';
+
+export default function useAuthContext() {
+  return useContext(IsLoggedContext);
+}
