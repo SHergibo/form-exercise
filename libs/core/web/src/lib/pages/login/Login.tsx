c66501bb5883342d2815ce132a733c03e8556971
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-20 15:02:52 +0200

    refactor(login): used FormProvider from react-hook-form

--- libs/core/web/src/lib/pages/login/Login.tsx
+++ libs/core/web/src/lib/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { FormProvider, useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
@@ -24,11 +24,16 @@ export function Login() {
   const [isError, setIsError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const methods = useForm<FormValues>({
+    defaultValues: { email: '', password: '' },
+    resolver: yupResolver(loginValidations),
+  });
+
   const {
-    register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>({ resolver: yupResolver(loginValidations) });
+    control,
+  } = methods;
 
   const onSubmit: SubmitHandler<FormValues> = async (data): Promise<void> => {
     setIsError(false);
@@ -58,62 +63,62 @@ export function Login() {
       sx={{ minHeight: '100vh' }}
     >
       <Grid item xs={3}>
-        <Typography variant="h2" component="div">
-          {t(i18nKeys.title.login)}
-        </Typography>
+        <Typography variant="h2">{t(i18nKeys.title.login)}</Typography>
 
-        <Box
-          component="form"
-          sx={{
-            minWidth: '20rem',
-          }}
-        >
-          <Grid container direction="column" spacing={1}>
-            <Grid item>
-              <InputTextField
-                size={EnumSize.small}
-                error={
-                  errors?.email?.message ? t(`${errors?.email?.message}`) : ''
-                }
-                id="email"
-                type="email"
-                label={`${t(i18nKeys.input.label.email)}`}
-                variant={EnumVariant.outlined}
-                required={true}
-                register={register}
-              />
+        <FormProvider {...methods}>
+          <Box
+            component="form"
+            sx={{
+              minWidth: '20rem',
+            }}
+          >
+            <Grid container direction="column" spacing={1}>
+              <Grid item>
+                <InputTextField
+                  size={EnumSize.small}
+                  error={
+                    errors?.email?.message ? t(`${errors?.email?.message}`) : ''
+                  }
+                  id="email"
+                  type="email"
+                  label={`${t(i18nKeys.input.label.email)}`}
+                  variant={EnumVariant.outlined}
+                  required={true}
+                  control={control}
+                />
+              </Grid>
+              <Grid item>
+                <InputTextField
+                  size={EnumSize.small}
+                  error={
+                    errors?.password?.message
+                      ? t(`${errors?.password?.message}`)
+                      : ''
+                  }
+                  id="password"
+                  type="password"
+                  label={`${t(i18nKeys.input.label.password)}`}
+                  variant={EnumVariant.outlined}
+                  required={true}
+                  control={control}
+                />
+              </Grid>
+              <Grid item>
+                <LoadingButton
+                  variant="outlined"
+                  type="submit"
+                  fullWidth
+                  onClick={handleSubmit(onSubmit)}
+                  loading={loading}
+                  endIcon={<LockOpenIcon />}
+                  loadingPosition="end"
+                >
+                  {t(i18nKeys.button.login)}
+                </LoadingButton>
+              </Grid>
             </Grid>
-            <Grid item>
-              <InputTextField
-                size={EnumSize.small}
-                error={
-                  errors?.password?.message
-                    ? t(`${errors?.password?.message}`)
-                    : ''
-                }
-                id="password"
-                type="password"
-                label={`${t(i18nKeys.input.label.password)}`}
-                variant={EnumVariant.outlined}
-                required={true}
-                register={register}
-              />
-            </Grid>
-            <Grid item>
-              <LoadingButton
-                variant="outlined"
-                type="submit"
-                fullWidth
-                onClick={handleSubmit(onSubmit)}
-                loading={loading}
-                endIcon={<LockOpenIcon />}
-                loadingPosition="end"
-              >
-                {t(i18nKeys.button.login)}
-              </LoadingButton>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        </FormProvider>
 
         <ErrorMessage message={isError ? t(i18nKeys.error.login) : ''} />
       </Grid>
