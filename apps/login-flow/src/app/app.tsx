920574fd813e945909f6a90e5795a5f43d4766ae
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-20 13:22:38 +0200

    refactor(app.tsx): used getRoutePath to search path for route

--- apps/login-flow/src/app/app.tsx
+++ apps/login-flow/src/app/app.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Admin, Login, NotFound } from '@form-exercise/core/web';
+import {
+  Admin,
+  AppRoute,
+  getRoutePath,
+  Login,
+  NotFound,
+} from '@form-exercise/core/web';
 import i18n from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import { MenuApp } from '@form-exercise/core/web';
 import ErrorBoundary from 'libs/core/web/src/lib/error-handler/error-boundary/ErrorBoundary';
 import { initI18Next } from '@form-exercise/i18n';
 import { ThemeContext } from '@form-exercise/ui';
-import { routePath } from '@form-exercise/utils';
 
 initI18Next();
 
@@ -17,9 +22,9 @@ export function App() {
         <ThemeContext>
           <Routes>
             <Route element={<MenuApp />}>
-              <Route path={routePath.login} element={<Login />} />
+              <Route path={getRoutePath(AppRoute.LOGIN)} element={<Login />} />
               <Route
-                path={routePath.admin}
+                path={getRoutePath(AppRoute.ADMIN)}
                 element={
                   <ErrorBoundary>
                     <Admin />
