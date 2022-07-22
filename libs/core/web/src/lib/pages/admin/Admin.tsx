fe813bf76d0cd830f5e791142cfae749ba5bf081
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-22 14:48:29 +0200

    refactor(admin): moved logout interaction to MenuApp

--- libs/core/web/src/lib/pages/admin/Admin.tsx
+++ libs/core/web/src/lib/pages/admin/Admin.tsx
@@ -1,40 +1,13 @@
 import Box from '@mui/material/Box';
-import LoadingButton from '@mui/lab/LoadingButton';
 import Typography from '@mui/material/Typography';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import LockIcon from '@mui/icons-material/Lock';
 import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import { i18nKeys } from '@form-exercise/i18n';
-import { ErrorMessage } from '@form-exercise/ui';
-import { useAuthContext } from '@form-exercise/utils';
-import { AppRoute, getRoutePath } from '../../routes';
 
 export function Admin() {
-  const { setIsLogged } = useAuthContext();
   const { t } = useTranslation();
-  const navigate = useNavigate();
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
   const [fakeError, setFakeError] = useState(false);
-
-  const logout = async (): Promise<void> => {
-    setLoading(true);
-    await axios
-      .post('/api/logout')
-      .then((response) => {
-        if (response.status === 200) {
-          setIsLogged(false);
-          navigate(getRoutePath(AppRoute.LOGIN));
-        }
-      })
-      .catch((error) => {
-        setError(t(i18nKeys.error.logout));
-        setLoading(false);
-      });
-  };
 
   useEffect(() => {
     if (fakeError) {
@@ -45,18 +18,6 @@ export function Admin() {
   return (
     <Box component="div">
       <Typography variant="h2">{t(i18nKeys.menu.title.admin)}</Typography>
-
-      <LoadingButton
-        variant="outlined"
-        onClick={logout}
-        loading={loading}
-        endIcon={<LockIcon />}
-        loadingPosition="end"
-      >
-        {t(i18nKeys.button.logout)}
-      </LoadingButton>
-
-      <ErrorMessage message={error} />
 
       <Button
         variant="contained"
