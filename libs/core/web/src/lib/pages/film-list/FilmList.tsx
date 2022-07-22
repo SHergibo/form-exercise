5d5f05103c01175e9cb5449c3c858808ffa4df37
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-22 13:05:26 +0200

    feat(filmlist): fetched data for films and added loop to show cards infos

--- libs/core/web/src/lib/pages/film-list/FilmList.tsx
+++ libs/core/web/src/lib/pages/film-list/FilmList.tsx
@@ -1,28 +1,66 @@
 import { i18nKeys } from '@form-exercise/i18n';
-import { Typography } from '@mui/material';
-import axios from 'axios';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import axios, { AxiosError } from 'axios';
 import { useTranslation } from 'react-i18next';
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
+
+interface Film {
+  Poster: string;
+  Title: string;
+  Type: string;
+  Year: string;
+  imdbID: string;
+}
+
+interface Films {
+  Reponse: string;
+  Search: Film[];
+  totalResults: string;
+}
 
 export function FilmList() {
-  const queryClient = useQueryClient();
   const { t } = useTranslation();
 
-  const getFilms = async () => {
-    await axios(
-      'http://www.omdbapi.com/?apikey=f9985b63&s=Batman&type=movie'
-    ).then((response) => {
-      console.log(response);
-      return response.data;
-    });
+  const getFilms = async (): Promise<Films> => {
+    const { data } = await axios(
+      `http://www.omdbapi.com/?apikey=${process.env['NX_OMDB_API_KEY']}&s=Batman&type=movie`
+    );
+    return data;
   };
 
-  const films = useQuery('films', getFilms);
+  const { isLoading, data: films } = useQuery<Films, AxiosError>(
+    'films',
+    getFilms
+  );
 
   return (
-    <div>
+    <>
       <Typography variant="h2">{t(i18nKeys.title.filmList)}</Typography>
-    </div>
+      {!isLoading && (
+        <Grid container spacing={2}>
+          {films?.Search.map((film: Film) => (
+            <Grid item xs={2} key={film.imdbID}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  image={film.Poster}
+                  alt={film.Title}
+                />
+                <CardContent>
+                  <Typography variant="h6">
+                    {film.Title} - {film.Year}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </>
   );
 }
 
