c5f51cd3501dba618699f5e3f9e7724058f0cb11
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-22 11:03:41 +0200

    feat(app.tsx): added QueryClientRoute and FilmList route

--- apps/login-flow/src/app/app.tsx
+++ apps/login-flow/src/app/app.tsx
@@ -9,16 +9,16 @@ import {
   MenuApp,
   ProtectedRoute,
   IsLoggedRoute,
+  FilmList,
+  QueryClientRoute,
 } from '@form-exercise/core/web';
 import i18n from 'i18next';
 import { I18nextProvider } from 'react-i18next';
 import { ErrorBoundary } from '@form-exercise/core/web';
 import { initI18Next } from '@form-exercise/i18n';
 import { ThemeContext } from '@form-exercise/ui';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 initI18Next();
-const queryClient = new QueryClient();
 
 export function App() {
   return (
@@ -26,29 +26,31 @@ export function App() {
       <BrowserRouter>
         <AuthContext>
           <ThemeContext>
-            <Routes>
-              <Route element={<MenuApp />}>
-                <Route element={<IsLoggedRoute />}>
-                  <Route
-                    path={getRoutePath(AppRoute.LOGIN)}
-                    element={<Login />}
-                  />
+            <ErrorBoundary>
+              <Routes>
+                <Route element={<MenuApp />}>
+                  <Route element={<IsLoggedRoute />}>
+                    <Route
+                      path={getRoutePath(AppRoute.LOGIN)}
+                      element={<Login />}
+                    />
+                  </Route>
+                  <Route element={<ProtectedRoute />}>
+                    <Route element={<QueryClientRoute />}>
+                      <Route
+                        path={getRoutePath(AppRoute.ADMIN)}
+                        element={<Admin />}
+                      ></Route>
+                      <Route
+                        path={getRoutePath(AppRoute.FILMLIST)}
+                        element={<FilmList />}
+                      />
+                    </Route>
+                  </Route>
+                  <Route path="*" element={<NotFound />} />
                 </Route>
-                <Route element={<ProtectedRoute />}>
-                  <Route
-                    path={getRoutePath(AppRoute.ADMIN)}
-                    element={
-                      <ErrorBoundary>
-                        <QueryClientProvider client={queryClient}>
-                          <Admin />
-                        </QueryClientProvider>
-                      </ErrorBoundary>
-                    }
-                  />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
+              </Routes>
+            </ErrorBoundary>
           </ThemeContext>
         </AuthContext>
       </BrowserRouter>
