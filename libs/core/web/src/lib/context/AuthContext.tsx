91eeb3c47acb72131aa052bd368edab5ff016aa6
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-22 16:05:11 +0200

    feat(authcontext): moved logout function in AuthContext and passed it to IsLoggedContext provider

--- libs/core/web/src/lib/context/AuthContext.tsx
+++ libs/core/web/src/lib/context/AuthContext.tsx
@@ -1,3 +1,4 @@
+import axios, { AxiosResponse } from 'axios';
 import {
   createContext,
   Dispatch,
@@ -5,6 +6,8 @@ import {
   SetStateAction,
   useState,
 } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getRoutePath, AppRoute } from '@form-exercise/core/web';
 
 export interface IAuthContext {
   children: ReactElement;
@@ -13,14 +16,34 @@ export interface IAuthContext {
 interface ContextValue {
   isLogged: boolean;
   setIsLogged: Dispatch<SetStateAction<boolean>>;
+  logout: () => Promise<AxiosResponse>;
 }
-const defaultValue = { isLogged: false, setIsLogged: () => null };
+const defaultValue = {
+  isLogged: false,
+  setIsLogged: () => null,
+  logout: () => ({} as Promise<AxiosResponse>),
+};
+
 export const IsLoggedContext = createContext<ContextValue>(defaultValue);
 
+export const login = () => {};
+
 export function AuthContext({ children }: IAuthContext) {
+  let navigate = useNavigate();
   const [isLogged, setIsLogged] = useState(false);
+
+  const logout = () => {
+    return axios.post('/api/logout').then((response) => {
+      if (response.status === 200) {
+        setIsLogged(false);
+        navigate(getRoutePath(AppRoute.LOGIN));
+      }
+      return response;
+    });
+  };
+
   return (
-    <IsLoggedContext.Provider value={{ isLogged, setIsLogged }}>
+    <IsLoggedContext.Provider value={{ isLogged, setIsLogged, logout }}>
       {children}
     </IsLoggedContext.Provider>
   );
