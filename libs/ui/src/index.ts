9888135ddc6cee06ff2e8e9220dce551c0cd0bd4
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-22 16:55:03 +0200

    refactor(card libs ui): renamed small-info-card folder to card folder

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,5 +1,5 @@
 export * from './components/icon-button-styled/IconButtonStyled';
-export * from './components/small-info-card/SmallInfoCard';
+export * from './components/card/SmallInfoCard';
 export * from './context/theme-context/ThemeContext';
 export * from './components/error-message/ErrorMessage';
 export * from './components/fields/input-text-field/InputTextField';
