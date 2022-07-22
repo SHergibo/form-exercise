452891929b6949f1bfc17f1d6edb8221a54a2fcf
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-22 11:05:51 +0200

    feat(locales): added translations for film list

--- libs/i18n/src/lib/keys.ts
+++ libs/i18n/src/lib/keys.ts
@@ -18,6 +18,7 @@ export const i18nKeys = {
       login: 'menu.title.login',
       admin: 'menu.title.admin',
       notFound: 'menu.title.notFound',
+      filmList: 'menu.title.filmList',
     },
   },
   title: {
@@ -25,6 +26,7 @@ export const i18nKeys = {
     admin: 'title.admin',
     notFound: 'title.notFound',
     boundaryError: 'title.boundaryError',
+    filmList: 'title.filmList',
   },
   validation: {
     required: 'validation.required',
