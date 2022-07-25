ee9258037c2abc58a3b48396d61b9e47e77126af
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-25 16:36:05 +0200

    feat(app.po.ts e2e support): added lng param to t function

--- apps/login-flow-e2e/src/support/app.po.ts
+++ apps/login-flow-e2e/src/support/app.po.ts
@@ -1,5 +1,5 @@
 import { Languages } from '@form-exercise/i18n';
 import { getI18n } from 'react-i18next';
 
-export const getGreeting = () => cy.get('h1');
-export const t = (key: string) => getI18n().t(key, { lng: Languages.FR });
+export const t = (key: string, lng: string = Languages.FR) =>
+  getI18n().t(key, { lng });
