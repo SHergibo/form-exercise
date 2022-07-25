e31ec75826032aa8537a15170c9c5951b28ef4a3
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-25 16:04:48 +0200

    feat(login.spec.ts): added tests for good and bad flow for login form

--- apps/login-flow-e2e/src/integration/login.spec.ts
+++ apps/login-flow-e2e/src/integration/login.spec.ts
@@ -3,16 +3,58 @@ import { t } from '../support/app.po';
 
 initI18Next();
 
-describe('Login', () => {
+describe('Login form', () => {
   beforeEach(() => cy.visit('/'));
+  describe('good flow', () => {
+    it('Test login with good credentials', () => {
+      cy.intercept('POST', '/api/login', {
+        status: 200,
+      });
 
-  it('Test login', () => {
-    cy.get('input[type=email]').type('test@test.com');
+      cy.fixture('login').then((fixture) => {
+        cy.get('input[type=email]').type(fixture.goodEmail);
 
-    cy.get('input[type=password]').type('12345678');
+        cy.get('input[type=password]').type(fixture.goodPassword);
 
-    cy.get('button[type=submit]')
-      .should('contain.text', t(i18nKeys.button.login))
-      .click();
+        cy.get('button[type=submit]')
+          .should('contain.text', t(i18nKeys.button.login))
+          .click();
+
+        cy.get('h6').should('contain.text', t(i18nKeys.menu.title.admin));
+      });
+    });
+  });
+  describe('bad flow', () => {
+    it('Test login with good email but wrong password', () => {
+      cy.intercept('POST', '/api/login', {
+        statusCode: 401,
+      });
+      cy.fixture('login').then((fixture) => {
+        cy.get('input[type=email]').type(fixture.goodEmail);
+
+        cy.get('input[type=password]').type(fixture.badPassword);
+
+        cy.get('button[type=submit]')
+          .should('contain.text', t(i18nKeys.button.login))
+          .click();
+
+        cy.get('p').should('contain.text', t(i18nKeys.error.login));
+      });
+    });
+    it('Test validation error message fields', function () {
+      cy.fixture('login').then((fixture) => {
+        cy.get('input[type=email]').focus();
+        cy.get('input[type=password]').focus();
+        cy.get('button[type=submit]').focus();
+        cy.get('p').should('contain.text', t(i18nKeys.validation.required));
+        cy.get('p').should('contain.text', t(i18nKeys.validation.invalid));
+
+        cy.get('input[type=email]').type(fixture.badEmail);
+        cy.get('p').should('contain.text', t(i18nKeys.validation.invalidEmail));
+
+        cy.get('input[type=password]').type(fixture.shortPassword);
+        cy.get('p').should('contain.text', t(i18nKeys.validation.invalid));
+      });
+    });
   });
 });
