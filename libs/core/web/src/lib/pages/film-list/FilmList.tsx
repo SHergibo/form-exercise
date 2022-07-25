3daad44748807a4b9497ea8986856631e2a2325f
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-25 08:52:21 +0200

    refactor(filmlist): changed Film to Movie from data/interface lib

--- libs/core/web/src/lib/pages/film-list/FilmList.tsx
+++ libs/core/web/src/lib/pages/film-list/FilmList.tsx
@@ -4,18 +4,18 @@ import Grid from '@mui/material/Grid';
 import { useTranslation } from 'react-i18next';
 import { SmallInfoCard } from '@form-exercise/ui';
 import { useFetchOmdb } from '@form-exercise/utils';
-import { Film } from '@form-exercise/data/interface';
+import { Movie } from '@form-exercise/data/interface';
 
 export function FilmList() {
   const { t } = useTranslation();
-  const { isLoading, data: films } = useFetchOmdb({ searchBy: 'Batman' });
+  const { isLoading, data: movies } = useFetchOmdb({ searchBy: 'Batman' });
 
   return (
     <>
       <Typography variant="h2">{t(i18nKeys.title.filmList)}</Typography>
       {!isLoading && (
         <Grid container spacing={2}>
-          {films?.Search.map(({ Poster, Title, Year, imdbID }: Film) => (
+          {movies?.Search.map(({ Poster, Title, Year, imdbID }: Movie) => (
             <Grid item xs={2} key={imdbID}>
               <SmallInfoCard
                 image={Poster}
