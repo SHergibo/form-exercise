c6290e4628cce0d477016a37df0197034d6646ef
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-25 13:40:53 +0200

    refactor(iconbuttonstyled): deleted unused IconButtonStyled component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,3 @@
-export * from './components/icon-button-styled/IconButtonStyled';
 export * from './components/card/SmallInfoCard';
 export * from './context/theme-context/ThemeContext';
 export * from './components/error-message/ErrorMessage';
