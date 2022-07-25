6a1163736ac3463b252b70e352aaaf2f37f6284d
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-25 10:50:10 +0200

    feat(movieslist): added search form to search movies

--- libs/core/web/src/lib/pages/movies-list/MoviesList.tsx
+++ libs/core/web/src/lib/pages/movies-list/MoviesList.tsx
@@ -2,18 +2,80 @@ import { i18nKeys } from '@form-exercise/i18n';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import CircularProgress from '@mui/material/CircularProgress';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SearchIcon from '@mui/icons-material/Search';
 import { useTranslation } from 'react-i18next';
-import { SmallInfoCard } from '@form-exercise/ui';
+import { InputTextField, SmallInfoCard } from '@form-exercise/ui';
 import { useGetMovies } from '@form-exercise/utils';
 import { Movie } from '@form-exercise/data/interface';
+import { EnumSize, EnumVariant } from '@form-exercise/data/enum';
+import { FormProvider, useForm, SubmitHandler } from 'react-hook-form';
+import { useState } from 'react';
+
+interface FormValues {
+  movieSearch: string;
+}
 
 export function MoviesList() {
   const { t } = useTranslation();
-  const { isLoading, data: movies } = useGetMovies({ searchBy: 'Batman' });
+  const [searchMoviesBy, setSearchMoviesBy] = useState('');
+  const { isLoading, data: movies } = useGetMovies({
+    searchBy: searchMoviesBy,
+  });
+
+  const methods = useForm<FormValues>({
+    defaultValues: { movieSearch: '' },
+    mode: 'all',
+  });
+
+  const { handleSubmit } = methods;
+
+  const searchMovies: SubmitHandler<FormValues> = (data): void => {
+    setSearchMoviesBy(data.movieSearch);
+  };
 
   return (
     <>
-      <Typography variant="h2">{t(i18nKeys.title.moviesList)}</Typography>
+      <Grid container spacing={2} direction="row" component="div">
+        <Grid item>
+          <Typography variant="h2">{t(i18nKeys.title.moviesList)}</Typography>
+        </Grid>
+        <Grid item display="flex" alignItems="center">
+          <FormProvider {...methods}>
+            <Grid
+              container
+              columnSpacing={2}
+              component="form"
+              direction="row"
+              alignItems="center"
+              onSubmit={handleSubmit(searchMovies)}
+            >
+              <Grid item>
+                <InputTextField
+                  size={EnumSize.small}
+                  id="movieSearch"
+                  type="movieSearch"
+                  label={`${t(i18nKeys.input.label.movie)}`}
+                  variant={EnumVariant.outlined}
+                  helperTextMessage={false}
+                />
+              </Grid>
+              <Grid item>
+                <LoadingButton
+                  variant="outlined"
+                  type="submit"
+                  fullWidth
+                  loading={isLoading}
+                  endIcon={<SearchIcon />}
+                  loadingPosition="end"
+                >
+                  {t(i18nKeys.button.search)}
+                </LoadingButton>
+              </Grid>
+            </Grid>
+          </FormProvider>
+        </Grid>
+      </Grid>
       {isLoading ? (
         <Grid
           container
@@ -29,15 +91,16 @@ export function MoviesList() {
         </Grid>
       ) : (
         <Grid container spacing={2}>
-          {movies?.Search.map(({ Poster, Title, Year, imdbID }: Movie) => (
-            <Grid item xs={2} key={imdbID}>
-              <SmallInfoCard
-                image={Poster}
-                alt={Title}
-                title={`${Title} - ${Year}`}
-              />
-            </Grid>
-          ))}
+          {movies &&
+            movies?.Search.map(({ Poster, Title, Year, imdbID }: Movie) => (
+              <Grid item xs={2} key={imdbID}>
+                <SmallInfoCard
+                  image={Poster}
+                  alt={Title}
+                  title={`${Title} - ${Year}`}
+                />
+              </Grid>
+            ))}
         </Grid>
       )}
     </>
