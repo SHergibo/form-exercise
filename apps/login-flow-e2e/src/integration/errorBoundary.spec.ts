fd12c10d2075be92ddf2ae4e245a08597cc9f7fd
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-26 15:44:02 +0200

    refactor(errorboundary): used contains method instead of using one string with get

--- apps/login-flow-e2e/src/integration/errorBoundary.spec.ts
+++ apps/login-flow-e2e/src/integration/errorBoundary.spec.ts
@@ -7,7 +7,7 @@ describe('ErrorBoundary', () => {
   beforeEach(() => {
     cy.login();
     cy.visit('/admin');
-    cy.get(`button:contains(${t(i18nKeys.button.generateAnError)})`).click();
+    cy.get('button').contains(t(i18nKeys.button.generateAnError)).click();
     Cypress.on('uncaught:exception', () => {
       return false;
     });
@@ -16,14 +16,14 @@ describe('ErrorBoundary', () => {
     cy.get('h2').should('contain.text', t(i18nKeys.title.boundaryError));
   });
   it('Test click on return last page ', () => {
-    cy.get(`button:contains(${t(i18nKeys.button.returnLastPage)})`).click({
+    cy.get('button').contains(t(i18nKeys.button.returnLastPage)).click({
       force: true,
     });
 
     cy.get('h6').should('contain.text', t(i18nKeys.title.admin));
   });
   it('Test click on return to login page ', () => {
-    cy.get(`button:contains(${t(i18nKeys.button.returnLogin)})`).click({
+    cy.get('button').contains(t(i18nKeys.button.returnLogin)).click({
       force: true,
     });
 
