dad0e02aa7adb351aa163e67e92b593946348177
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-26 16:49:44 +0200

    refactor(menuapp): refactored nav element

--- libs/core/web/src/lib/layout/menu/MenuApp.tsx
+++ libs/core/web/src/lib/layout/menu/MenuApp.tsx
@@ -2,12 +2,15 @@ import { useState, MouseEvent } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 import Box from '@mui/material/Box';
 import { useTranslation } from 'react-i18next';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { i18nKeys, Languages, languageSwitcher } from '@form-exercise/i18n';
@@ -40,21 +43,20 @@ export function MenuApp() {
             <MenuTitle />
 
             {isLogged && (
-              <Box display="flex" flexDirection="row">
+              <List component="nav">
                 {adminRoutesArray.map((route, index) => (
                   <Button
+                    component={NavLink}
+                    to={route.path}
                     key={`${route.routeName}-${index}`}
-                    onClick={() => {
-                      navigate(route.path);
-                    }}
                     color="white"
                   >
-                    <Typography textAlign="center">
-                      {t(route.routeName)}
-                    </Typography>
+                    <ListItemButton>
+                      <ListItemText primary={t(route.routeName)} />
+                    </ListItemButton>
                   </Button>
                 ))}
-              </Box>
+              </List>
             )}
 
             <Button
