ea908eb9a431d8d1d597ed66e7355c3ba19d60a4
Sacha <sacha@sachas-mbp.intersysto.local>
2022-07-26 11:34:35 +0200

    feat(login-flow-e2e commands): updated custom login command for cypress

--- apps/login-flow-e2e/src/support/commands.ts
+++ apps/login-flow-e2e/src/support/commands.ts
@@ -12,13 +12,13 @@
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    login(): void;
   }
 }
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+Cypress.Commands.add('login', () => {
+  sessionStorage.setItem('isLogged', 'true');
 });
 //
 // -- This is a child command --
